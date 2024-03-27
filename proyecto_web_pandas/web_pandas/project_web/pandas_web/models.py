ce1c9080a8a0926e1b6052bace2cf5dba869e677
RicardoBarrioB <162453002+RicardoBarrioB@users.noreply.github.com>
2024-03-27 18:46:27 +0100

    pandas_web

---
+++ proyecto_web_pandas/web_pandas/project_web/pandas_web/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib import admin
+
+class DataSet(models.Model):
+    name = models.CharField(max_length=100)
+    upload_date = models.DateTimeField(auto_now_add=True)
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class DataColumn(models.Model):
+    name = models.CharField(max_length=100)
+    data_type = models.CharField(max_length=50)
+    data_set = models.ForeignKey(DataSet, on_delete = models.CASCADE)
+
+class DataPoint(models.Model):
+    column = models.ForeignKey(DataColumn, on_delete=models.CASCADE)
+    value = models.FloatField()
+
+class AnalysisResult(models.Model):
+    ANALYSIS_TYPES = [
+        ('1', 'Descripción estadística'),
+        ('2', 'Visualización de datos'),
+        ('3', 'Análisis específico'),
+        # Agrega más tipos de análisis según sea necesario
+    ]
+
+    analysis_type = models.CharField(max_length=1, choices=ANALYSIS_TYPES)
+    parameters = models.JSONField()
+    result_data = models.TextField()
+    analyzed_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    analysis_date = models.DateTimeField(auto_now_add=True)
